f429dbebe6a2309f09969fc25a044797ef78fa5d
Christian Schiffler <c.schiffler@cyberspectrum.de>
2016-05-04 12:02:59 +0200

    Bump phpcq and fix issues

--- contao/languages/en/default.php
+++ contao/languages/en/default.php
@@ -1,24 +1,27 @@
 <?php
+
 /**
- * The MetaModels extension allows the creation of multiple collections of custom items,
- * each with its own unique set of selectable attributes, with attribute extendability.
- * The Front-End modules allow you to build powerful listing and filtering of the
- * data in each collection.
+ * This file is part of MetaModels/attribute_translatedcheckbox.
  *
- * PHP version 5
+ * (c) 2012-2016 The MetaModels team.
  *
- * @package     MetaModels
- * @subpackage  AttributeTranslatedCheckbox
- * @author      Christian Schiffler <c.schiffler@cyberspectrum.de>
- * @copyright   The MetaModels team.
- * @license     LGPL.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    MetaModels
+ * @subpackage AttributeTranslatedCheckbox
+ * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @copyright  2012-2016 The MetaModels team.
+ * @license    https://github.com/MetaModels/attribute_translatedcheckbox/blob/master/LICENSE LGPL-3.0
  * @filesource
  */
 
-/**
- * Miscellaneous
- */
-$GLOBALS['TL_LANG']['MSC']['metamodel_filtersetting']['ignore_published']   = array('Ignore published field %s', 'If you check this, the published condition for attribute %s will not be enforced in the filter.');
+$GLOBALS['TL_LANG']['MSC']['metamodel_filtersetting']['ignore_published'][0] = 'Ignore published field %s';
+$GLOBALS['TL_LANG']['MSC']['metamodel_filtersetting']['ignore_published'][1] =
+    'If you check this, the published condition for attribute %s will not be enforced in the filter.';
 
 $GLOBALS['TL_LANG']['MSC']['metamodelattribute_translatedcheckbox']['toggle'][0] = 'Toggle';
-$GLOBALS['TL_LANG']['MSC']['metamodelattribute_translatedcheckbox']['toggle'][1] = 'Toggle the state of attribute %s for this item in language %s';
+$GLOBALS['TL_LANG']['MSC']['metamodelattribute_translatedcheckbox']['toggle'][1] =
+    'Toggle the state of attribute %s for this item in language %s';
