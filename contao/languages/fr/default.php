8f9ee750ec09e512e2085d9f09b0d44517b3ac29
Mini Model <minimodel@metamodel.me>
2017-05-25 00:08:34 +0200

    Updated translations from transifex.

--- contao/languages/fr/default.php
+++ contao/languages/fr/default.php
@@ -8,11 +8,10 @@
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2015-07-11T04:13:38+02:00
+ * last-updated: 2017-05-25T00:08:26+02:00
  */
 
 $GLOBALS['TL_LANG']['MSC']['metamodel_filtersetting']['ignore_published']['0']     = 'Ignorer le champ publié %s';
 $GLOBALS['TL_LANG']['MSC']['metamodel_filtersetting']['ignore_published']['1']     = 'Si coché, la condition publiée pour l\'attribut %s ne sera pas appliquée dans le filtre.';
 $GLOBALS['TL_LANG']['MSC']['metamodelattribute_translatedcheckbox']['toggle']['0'] = 'Basculer';
-$GLOBALS['TL_LANG']['MSC']['metamodelattribute_translatedcheckbox']['toggle']['1'] = 'Basculer %s';
 
