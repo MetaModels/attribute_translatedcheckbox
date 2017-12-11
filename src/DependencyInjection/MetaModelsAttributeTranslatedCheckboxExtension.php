70bb53be5d2dbdfece8e0315f0c42898b509126b
David Molineus <david.molineus@netzmacht.de>
2017-12-11 17:32:19 +0100

    Fix service registration.

--- src/DependencyInjection/MetaModelsAttributeTranslatedCheckboxExtension.php
+++ src/DependencyInjection/MetaModelsAttributeTranslatedCheckboxExtension.php
@@ -37,5 +37,6 @@ class MetaModelsAttributeTranslatedCheckboxExtension extends Extension
     {
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yml');
+        $loader->load('listeners.yml');
     }
 }
