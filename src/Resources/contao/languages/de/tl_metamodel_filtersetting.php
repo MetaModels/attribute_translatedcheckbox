b0511bd0a09ba58a8e6e1ea6466d81ebd77a3960
Mini Model <minimodel@metamodel.me>
2019-05-07 09:39:52 +0200

    Updated translations from transifex.

--- src/Resources/contao/languages/de/tl_metamodel_filtersetting.php
+++ src/Resources/contao/languages/de/tl_metamodel_filtersetting.php
@@ -1,19 +1,30 @@
 <?php
 /**
- * Translations are managed using Transifex. To create a new translation
+ * This file is part of MetaModels/attribute_translatedcheckbox.
+ *
+ * (c) 2012-2019 The MetaModels team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * Translations are managed automatically using Transifex. To create a new translation
  * or to help to maintain an existing one, please register at transifex.com.
  *
- * @link http://help.transifex.com/intro/translating.html
- * @link https://www.transifex.com/projects/p/metamodels/language/de/
+ * Last-updated: 2019-05-07T09:39:48+02:00
  *
- * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
- *
- * last-updated: 2013-07-08T08:16:40+02:00
+ * @copyright 2012-2019 The MetaModels team.
+ * @license   https://github.com/MetaModels/attribute_translatedcheckbox/blob/master/LICENSE LGPL-3.0-or-later
+ * @link      https://www.transifex.com/metamodels/public/
+ * @link      https://www.transifex.com/signup/?join_project=metamodels
  */
 
+
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['check_allowpreview']['0']                   = 'Filter nicht in Frontendvorschau nutzen';
-$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['check_allowpreview']['1']                   = 'Falls ausgewählt wird dieser Filter nicht benutzt wenn ein Benutzer die Frontend-Vorschau nutzt. Die Option "Unveröffentlichte Elemente anzeigen" wird ignoriert.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['check_allowpreview']['1']                   = 'Falls ausgewählt, wird dieser Filter nicht benutzt, wenn ein Benutzer die Frontend-Vorschau nutzt. Die Option "Unveröffentlichte Elemente anzeigen" wird ignoriert.';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['check_ignorepublished']['0']                = 'Überschreiben erlauben';
-$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['check_ignorepublished']['1']                = 'Falls aktiviert können Filterparameter diese Einstellung überschreiben.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['check_ignorepublished']['1']                = 'Falls aktiviert, können Filterparameter diese Einstellung überschreiben.';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['typedesc']['translatedcheckbox_published']  = '%s <strong>%s</strong> %s <br /> für Attribut <em>%s</em>';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['typenames']['translatedcheckbox_published'] = 'Übersetzter Veröffentlichungsstatus';
+
