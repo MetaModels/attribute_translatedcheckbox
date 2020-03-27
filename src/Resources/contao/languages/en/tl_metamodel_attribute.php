3ba0a7c4e4a25603464fc6fa2a52967de596a450
e-spin <gitlab@e-spin.de>
2020-03-27 16:12:04 +0100

    hotfix independent language key fix core #1308

--- src/Resources/contao/languages/en/tl_metamodel_attribute.php
+++ src/Resources/contao/languages/en/tl_metamodel_attribute.php
@@ -1,10 +1,9 @@
 <?php
 
-
 /**
  * This file is part of MetaModels/attribute_translatedcheckbox.
  *
- * (c) 2012-2018 The MetaModels team.
+ * (c) 2012-2020 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -15,9 +14,13 @@
  * @subpackage AttributeTranslatedCheckbox
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
  * @author     Sven Baumann <baumann.sv@gmail.com>
- * @copyright  2012-2018 The MetaModels team.
+ * @author     Ingolf Steinhardt <info@e-spin.de>
+ * @copyright  2012-2020 The MetaModels team.
  * @license    https://github.com/MetaModels/attribute_translatedcheckbox/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
 
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['typeOptions']['translatedcheckbox'] = 'Translated checkbox';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['check_publish'][0]                  = 'Publishing checkbox';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['check_publish'][1]                  =
+    'If this is selected, the items will appear in lists in the frontend (you will get the green "eye" for free.';
